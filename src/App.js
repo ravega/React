c6e3552074d0618ed0701afd8ea15ec2c4d78bdb
Raul Vega <ravega@gmail.com>
2018-11-16 19:21:41 +0000

    First changes for book list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './bookstore.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+	<h1>Book store</h1>
+	<br></br>
+	<Menu />
+	<br></br>
+	<hr></hr>
+	<BookList />
       </div>
+    );
+  }
+}
+
+class Menu extends Component {
+  render() {
+    return (
+    <div>     
+      <input></input>
+      <input></input>
+      <button>Save book</button>
+    </div>
+    );
+  }
+}
+
+const List = ({items}) => (
+  items.map( (item) => (
+  <tr>
+    <td>{item}</td>
+    <td>{item}</td>
+  </tr>
+  ))
+);
+
+class BookList extends Component {
+
+  constructor(props) {
+    super(props);
+    //this.state = {books: [{name: "Java 8", price: 35}, {name: "Linux", price: 30}]};
+    //const books = [{name: "Java 8", price: 35}, {name: "Linux", price: 30}];
+    this.state = {
+	    aValue: 'TestMe',
+	    numbers: ['1', '2', '3', '4', '5']
+    };
+  }
+
+  render() {
+    const {numbers, aValue} = this.state;
+
+    return (
+    <div>
+     <table className="storeTable">
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+	  <List items={numbers} />
+     </table> 
+    </div>
     );
   }
 }
