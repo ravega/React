7882dcb0eb6318835354dd81a1c2df115bb8d512
Raul Vega <ravega@gmail.com>
2018-12-26 21:02:56 +0000

    Material icons and delete method added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Navigation from './Navigation';
+import BookList from './BookList';
+import Menu from './Menu';
 import './bookstore.css';
 
 class App extends Component {
@@ -13,92 +15,30 @@ class App extends Component {
   // Guarda estos valores en el state como una nueva lista y a ver si se actualiza la tabla.
 	// Mira las lisrtas como mergear un valor nuevo o como crear una lista nueva
   saveBook(name, price) {
-    console.log('Book saved:', name);
-    console.log('Book saved costs:', price);
-    console.log('Books:', this.state.books);
     let bookList = this.state.books;
     bookList.push({'name': name, 'price': price});
     this.setState({books: bookList});
   }
 
+  deleteBook(name) {
+    this.setState(prevState => ({
+        books: prevState.books.filter(el => el.name !== name )
+    }));
+  }
+
   render() {
     return (
       <div>
-      <Navigation/>
-      <div className="container">
-	<h1>Book store</h1>
-	<br></br>
-	<Menu save={this.saveBook.bind(this)}/>
-	<br></br>
-	<hr></hr>
-	<BookList books={this.state.books}/>
+        <Navigation/>
+        <div className="container">
+	        <h1>Book store</h1>
+	        <br></br>
+	        <Menu save={this.saveBook.bind(this)}/>
+	        <br></br>
+	        <hr></hr>
+	        <BookList books={this.state.books} remove={this.deleteBook.bind(this)}/>
+        </div>
       </div>
-      </div>
-    );
-  }
-}
-
-class Menu extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      bookName: '',
-      bookPrice: 0
-    };
-  }
-
-  updateInputName(evt) {
-    this.setState({
-      bookName: evt.target.value
-    });
-  }
-
-  updateInputPrice(evt) {
-    this.setState({
-      bookPrice: evt.target.value
-    });
-  }
-
-  saveBookHandler = () => {
-    this.props.save(this.state.bookName, this.state.bookPrice);
-  }
-
-  render() {
-    return (
-    <div>     
-      <input value={this.state.bookName} onChange={evt => this.updateInputName(evt)}></input>
-      <input value={this.state.bookPrice} onChange={evt => this.updateInputPrice(evt)}></input>
-      <button onClick={this.saveBookHandler.bind(this)}>Save book</button>
-    </div>
-    );
-  }
-}
-
-const List = ({items}) => (
-  items.map( (item) => (
-  <tr key={item.name.toString()}>
-    <td>{item.name}</td>
-    <td>{item.price}</td>
-  </tr>
-  ))
-);
-
-class BookList extends Component {
-
-  render() {
-    return (
-    <div>
-     <table className="storeTable">
-	<tbody>
-          <tr>
-            <th>Name</th>
-            <th>Price</th>
-          </tr>
-	  <List items={this.props.books} />
-	</tbody>
-     </table> 
-    </div>
     );
   }
 }
--- src/Navigation.js
+++ src/Navigation.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
+import MaterialIcon from 'material-icons-react';
 
 class Navigation extends React.Component {
   render() {
@@ -7,32 +8,24 @@ class Navigation extends React.Component {
 	<Navbar inverse collapseOnSelect>
 	  <Navbar.Header>
 	    <Navbar.Brand>
-	      <a href="#brand">React-Bootstrap</a>
+        <MaterialIcon icon="dashboard" color='#ffffff'/>
+	      <a href="#brand" className="navbarmb">The Store</a>
 	    </Navbar.Brand>
 	    <Navbar.Toggle />
 	  </Navbar.Header>
 	  <Navbar.Collapse>
-	    <Nav>
-	      <NavItem eventKey={1} href="#">
-	        Link
-	      </NavItem>
-	      <NavItem eventKey={2} href="#">
-	        Link
-	      </NavItem>
-	      <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-	        <MenuItem eventKey={3.1}>Action</MenuItem>
-	        <MenuItem eventKey={3.2}>Another action</MenuItem>
-	        <MenuItem eventKey={3.3}>Something else here</MenuItem>
-	        <MenuItem divider />
-	        <MenuItem eventKey={3.3}>Separated link</MenuItem>
-	      </NavDropdown>
-	    </Nav>
 	    <Nav pullRight>
-	      <NavItem eventKey={1} href="#">
-	        Link Right
-	      </NavItem>
+        <Nav>
+	        <NavDropdown eventKey={3} title="Language" id="basic-nav-dropdown">
+	          <MenuItem eventKey={3.1}>Español</MenuItem>
+	          <MenuItem eventKey={3.2}>English</MenuItem>
+	          <MenuItem eventKey={3.3}>Deutsche</MenuItem>
+	          <MenuItem divider />
+	          <MenuItem eventKey={3.3}>Others</MenuItem>
+	        </NavDropdown>
+	      </Nav>
 	      <NavItem eventKey={2} href="#">
-	        Link Right
+	        Log in
 	      </NavItem>
 	    </Nav>
 	  </Navbar.Collapse>
